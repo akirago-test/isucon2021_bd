2c129a599a6f73e7a8dc19c26c039a8f60070e4f
akirago <akirasyoku1234@gmail.com>
2022-07-22 20:47:27 +0900

    add shell script

---
+++ shell/before.sh
@@ -0,0 +1,9 @@
+alp json --sort sum -r -m "/api/condition/[0-z]+" -o count,method,uri,min,avg,max,sum < /home/isucon/webapp/log/access.log | /home/isucon/./notify_slack -snippet -filename access_log
+sudo rm /home/isucon/webapp/log/access.log
+sudo pt-query-digest /var/log/mysql/mariadb-slow.log --limit 10  | /home/isucon/./notify_slack -snippet -filename slow_query
+sudo rm /var/log/mysql/mariadb-slow.log
+
+sudo git pull
+
+sudo systemctl restart nginx
+sudo systemctl restart mariadb
